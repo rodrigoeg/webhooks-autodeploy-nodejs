877125921cd86d8c2fd2b77c2213eb13b187364e
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-06-07 18:06:39 -0300

    Changing branch name to staging

--- deploy_app.sh
+++ deploy_app.sh
@@ -4,6 +4,7 @@
 REPOSITORY=$(echo $1)
 BRANCH=$(echo $2 | sed -e "s/refs\/heads\///g")
 URL=$(echo $3)
+CHECKOUT_BRANCH=$(echo $4)
 
 # update or clone repository
 if [ "$BRANCH" == "$CHECKOUT_BRANCH" ]; then
