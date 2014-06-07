248d5514ced6abce49bba30d915aecc76d5a585e
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-06-07 18:31:50 -0300

    Fix checkout branch

--- deploy_app.sh
+++ deploy_app.sh
@@ -13,7 +13,7 @@ if [ "$BRANCH" == "$CHECKOUT_BRANCH" ]; then
     cd $1
     git fetch --all
     git reset --hard origin/$BRANCH
-    git checkout origin/$BRANCH
+    git checkout $BRANCH
     cd ..
   else
     git clone $URL -b $BRANCH
