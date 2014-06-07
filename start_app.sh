4b96a7c0e6b76691532d95ed68fb3a38446b73c2
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-06-07 16:08:14 -0300

    Start scripts

--- start_app.sh
+++ start_app.sh
@@ -7,10 +7,21 @@ URL=$(echo $3)
 CHECKOUT_BRANCH=$(echo $4)
 
 if [ "$BRANCH" == "$CHECKOUT_BRANCH" ]; then
-  forever start ./$REPOSITORY/cluster.js \
-    --server-port=80 \
-    --auth-uri=http://prj02.lasca.ic.unicamp.br:80 \
-    --calendar-uri=http://prj13.lasca.ic.unicamp.br:80 \
-    --courses-uri=http://prj04.lasca.ic.unicamp.br:80 \
-    --school-history-uri=http://prj08.lasca.ic.unicamp.br:80
+  if [ "$BRANCH" == "development" ]; then
+    forever start ./$REPOSITORY/cluster.js \
+      --server-port=80 \
+      --auth-uri=http://prj02.lasca.ic.unicamp.br:80 \
+      --calendar-uri=http://prj13.lasca.ic.unicamp.br:80 \
+      --courses-uri=http://prj04.lasca.ic.unicamp.br:80 \
+      --school-history-uri=http://prj08.lasca.ic.unicamp.br:80
+  fi
+
+  if [ "$BRANCH" == "master" ]; then
+    forever start ./$REPOSITORY/cluster.js \
+      --server-port=80 \
+      --auth-uri=http://prj01.lasca.ic.unicamp.br:80 \
+      --calendar-uri=http://prj14.lasca.ic.unicamp.br:80 \
+      --courses-uri=http://prj03.lasca.ic.unicamp.br:80 \
+      --school-history-uri=http://prj07.lasca.ic.unicamp.br:80
+  fi
 fi
